d234f73e9d121a944cbe879075715ad80a4e2dcb
Sungoh Park <spark449062@gmail.com>
2021-07-29 11:11:01 -0600

    all deliverables met

--- src/index.js
+++ src/index.js
@@ -1,1 +1,70 @@
-// write your code here
+const fetchImagesURL = "http://localhost:3000/images";
+const fetchCommentURL = "http://localhost:3000/comments";
+const likeButtonElement = document.querySelector("button#fg-like-button");
+const likeCountElement = document.querySelector("span#fg-likes");
+const commentInputElement = document.querySelector("input.comment-input")
+const commentButtonElement = document.querySelector("button.comment-button")
+const commentPostElementParent = document.querySelector("ul#fg-comments");
+const commentFormElement = document.querySelector("form#fg-comment-form")
+const imageTitleElement = document.querySelector("h2#fg-title");
+const imageContentElement = document.querySelector("img#fg-image");
+
+let likeCount = 0;
+let imageObjectArray = [];
+let commentObjectArray = [];
+fetch(fetchImagesURL)
+.then(response => response.json()) // json() is a function! remember to put ()
+.then(jsonObject => {
+    saveObjectArray(imageObjectArray, jsonObject)
+    listenLikeEvent();
+    listenPostEvent();
+    displayPostContent();
+    loadPreviousComments();
+});
+
+fetch(fetchCommentURL)
+.then(response => response.json()) // json() is a function! remember to put ()
+.then(jsonObject => {
+    saveObjectArray(commentObjectArray, jsonObject)
+});
+
+function saveObjectArray(saveTarget, jsonObjectFetched) {
+    for (let i = 0; i < jsonObjectFetched.length; i++) {
+        saveTarget.push(jsonObjectFetched[i]);
+    }
+}
+
+console.log("image object array is fetched:")
+console.log(imageObjectArray);
+console.log("comment object array is fetched:")
+console.log(commentObjectArray);
+
+function displayPostContent() {
+    imageTitleElement.innerText = imageObjectArray[0].title;
+    imageContentElement.src = imageObjectArray[0].image;
+}
+
+function loadPreviousComments() {
+    for (let i = 0; i < commentObjectArray.length; i++) {
+        let newPost = document.createElement("li");
+        newPost.innerText = commentObjectArray[i].content;
+        commentPostElementParent.append(newPost);
+    }
+}
+
+function listenLikeEvent() {
+    likeButtonElement.addEventListener("click", function() {
+        likeCount++;
+        likeCountElement.innerText = likeCount + " likes";
+    })
+}
+
+ function listenPostEvent() {
+     commentButtonElement.addEventListener("click", function(event) {
+        event.preventDefault();
+        let newPost = document.createElement("li");
+        newPost.innerText = commentInputElement.value;
+        commentPostElementParent.append(newPost);
+        commentFormElement.reset();
+     })
+ }
